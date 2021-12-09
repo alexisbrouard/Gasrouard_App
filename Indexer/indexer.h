a9792404bd3e196d202acf5d2deac59a857560b5
AlexisBrouardYnov <67193389+AlexisBrouardYnov@users.noreply.github.com>
2021-12-09 14:55:52 +0100

    first commit

---
+++ Indexer/indexer.h
@@ -0,0 +1,12 @@
+#ifndef INDEXER_H
+#define INDEXER_H
+
+#include "Indexer_global.h"
+
+class INDEXER_EXPORT Indexer
+{
+public:
+    Indexer();
+};
+
+#endif // INDEXER_H
---
+++ README.md
@@ -0,0 +1,2 @@
+# Gasrouard_App
+ 
